4dc8b52e95129aca5508c28d93c077d2fe9d6719
Lance Hunter <lancehunter@gmail.com>
2017-12-20 15:46:58 -0600

    Added basic info to routes.

---
+++ routes/accounts.js
@@ -0,0 +1,25 @@
+'use strict';
+
+//Setting up knex
+const env = 'development';
+const config = require('../knexfile.js')[env];
+const knex = require('knex')(config);
+
+//Setting up express routing
+const express = require('express');
+const router = express.Router();
+
+
+// filterInt - The function from MDN that confirms a particular value is actually an integer. Because parseInt isn't quite strict enough.
+const filterInt = function(value) {
+  if (/^(\-|\+)?([0-9]+|Infinity)$/.test(value))
+    return Number(value);
+  return NaN;
+};
+
+
+
+
+
+
+module.exports = router;
