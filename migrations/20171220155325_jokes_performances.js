32c59eff5558d979dd2ea126f29e8ad1fdb98426
Paige Throckmorton <path4942@gmail.com>
2017-12-20 16:08:05 -0600

    Second migration, jokes/people tables

---
+++ migrations/20171220155325_jokes_performances.js
@@ -0,0 +1,27 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTableIfNotExists('jokes', function(table){
+    table.increments('joke_id');
+    table.integer('user_id').references('users.id').onDelete('cascade');
+    table.string('joke_title').notNullable();
+  }).then(function(){
+    return knex.schema.createTableIfNotExists('performances', function(table){
+      table.increments('per_id');
+      table.integer('user_id').references('users.id').onDelete('cascade');
+      table.string('per_title').notNullable();
+      table.string('location');
+      table.integer('given_time').notNullable();
+      table.integer('per_time').notNullable();
+      table.string('audio');
+      table.integer('rating');
+    })
+  })
+
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('jokes')
+  .then(function(){
+    return knex.schema.dropTableIfExists('performances')
+  })
+};
