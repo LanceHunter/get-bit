c5443adf095b8de88e62af553c508625323f70be
Lance Hunter <lancehunter@gmail.com>
2018-01-05 10:31:16 -0600

    Fixed deploy database bug

--- knexfile.js
+++ knexfile.js
@@ -35,6 +35,7 @@ module.exports = {
   production: {
     client: 'pg',
     connection: {
+      host:     process.env.DATABASE_HOST,
       database: process.env.DATABASE,
       user:     process.env.DATABASE_USER,
       password: process.env.DATABASE_PASSWORD,
